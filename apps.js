618e84564a7fa742ef073afe7698f6fea8b6676c
Manatocode <gabrielmartinellix@outlook.com>
2024-05-07 17:30:08 -0300

    1

---
+++ apps.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const rotaLivro = require("./rotas/livros")
+
+const app = express();
+
+app.use('/livros', rotaLivro)
+
+const port = 8000;
+
+
+app.listen(port, () => {
+    console.log(`escutando a porta ${port}`)
+})
